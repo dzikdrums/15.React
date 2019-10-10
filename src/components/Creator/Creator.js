a8cafc45beaae076ff889e2796be197f731703de
user <user@users-MacBook-Air.local>
2019-10-10 08:32:18 +0200

    add delete confirmation

--- src/components/Creator/Creator.js
+++ src/components/Creator/Creator.js
@@ -1,59 +1,102 @@
-import React from 'react';
-import styles from './Creator.scss';
-import Button from '../Button/Button';
-import PropTypes from 'prop-types';
+import React from "react";
+import styles from "./Creator.scss";
+import Button from "../Button/Button";
+import PropTypes from "prop-types";
 
 class Creator extends React.Component {
   static propTypes = {
-    text: PropTypes.string,
-  }
+    text: PropTypes.string
+  };
 
   static defaultProps = {
-    text: 'Add new item',
-  }
+    text: "Add new item"
+  };
 
   state = {
-    value: '',
+    value: "",
     visibleButtons: false,
-  }
+    visibleButtonsCancel: false
+  };
 
-  handleChange(event){
-    // console.log(event);
+  handleChange(event) {
     this.setState({
       value: event.target.value,
       visibleButtons: event.target.value.length > 0
     });
   }
 
-  handleOK(){
-    if(this.state.value != ''){
+  handleOK() {
+    if (this.state.value != "") {
       this.props.action(this.state.value);
       this.setState({
-        value: '',
+        value: "",
         visibleButtons: false
       });
     }
   }
 
-  handleCancel(){
+  handleCancel() {
     this.setState({
-      value: '',
-      visibleButtons: false
+      visibleButtons: false,
+      visibleButtonsCancel: true
+    });
+  }
+
+  handleCancelYes() {
+    this.setState({
+      value: "",
+      visibleButtons: false,
+      visibleButtonsCancel: false
+    });
+  }
+
+  handleCancelNo() {
+    this.setState({
+      visibleButtons: true,
+      visibleButtonsCancel: false
     });
   }
 
   render() {
     return (
-      <div className={styles.component}>
+      <div
+        className={
+          styles.component +
+          (this.state.visibleButtonsCancel ? " " + styles.buttonsShown : "")
+        }
+      >
         <input
-          type='text'
+          type="text"
           placeholder={this.props.text}
           value={this.state.value}
           onChange={event => this.handleChange(event)}
         />
-        <div className={styles.buttons + (this.state.visibleButtons ? ' ' + styles.buttonsShown : '')}>
+        <div
+          className={
+            styles.buttons +
+            (this.state.visibleButtons ? " " + styles.buttonsShown : "")
+          }
+        >
           <Button onClick={() => this.handleOK()}>OK</Button>
-          <Button onClick={() => this.handleCancel()} variant='danger'>cancel</Button>
+          <Button onClick={() => this.handleCancel()} variant="danger">
+            cancel
+          </Button>
+        </div>
+        <div
+          className={
+            styles.buttons +
+            (this.state.visibleButtonsCancel
+              ? " " + styles.buttonsShown + " " + styles.flexColumn
+              : "")
+          }
+        >
+          <div className={styles.buttons + " " + styles.buttonsShown}>
+            <Button onClick={() => this.handleCancelYes()}>Yes</Button>
+            <Button onClick={() => this.handleCancelNo()} variant="danger">
+              No
+            </Button>
+          </div>
+          <p className={styles.confirmation}>Do You really want to cancel?</p>
         </div>
       </div>
     );
