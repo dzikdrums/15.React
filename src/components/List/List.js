bbf82617f103e91122a294d99f9130112a52c3e0
user <user@users-MacBook-Air.local>
2019-10-22 22:22:51 +0200

    fix Container component usage

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -24,17 +24,20 @@ class List extends React.Component {
     const { title, image, description, columns, addColumn } = this.props;
     return (
       <section className={styles.component}>
-        <Container />
-        <Hero titleText={title} image={image} />
-        <div className={styles.description}>{ReactHtmlParser(description)}</div>
-        <div className={styles.columns}>
-          {columns.map(columnData => (
-            <Column key={columnData.id} {...columnData} />
-          ))}
-        </div>
-        <div className={styles.creator}>
-          <Creator text={settings.columnCreatorText} action={addColumn} />
-        </div>
+        <Container>
+          <Hero titleText={title} image={image} />
+          <div className={styles.description}>
+            {ReactHtmlParser(description)}
+          </div>
+          <div className={styles.columns}>
+            {columns.map(columnData => (
+              <Column key={columnData.id} {...columnData} />
+            ))}
+          </div>
+          <div className={styles.creator}>
+            <Creator text={settings.columnCreatorText} action={addColumn} />
+          </div>
+        </Container>
       </section>
     );
   }
