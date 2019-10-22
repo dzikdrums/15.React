2d3496105ced5a461bc7f05bf919cd80b9640a78
user <user@users-MacBook-Air.local>
2019-10-22 17:35:20 +0200

    fix Container component

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import styles from './Container.scss';
+import PropTypes from 'prop-types';
 
-const Container = children => <div className={styles.component}></div>;
+const Container = props => (
+  <div className={styles.component}>{props.children}</div>
+);
+
+Container.propTypes = {
+  children: PropTypes.node,
+};
 
 export default Container;
