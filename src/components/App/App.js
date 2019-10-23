e8e5ee2f79d10cb2a63a725e7ad9f9222985fce5
user <user@users-MacBook-Air.local>
2019-10-23 14:16:56 +0200

    fix FAQ import in App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import Home from '../Home/HomeContainer';
 import Info from '../Info/Info';
-import Faq from '../Faq/Faq';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Faq from '../FAQ/Faq';
+import { BrowserRouter, Route } from 'react-router-dom';
 import MainLayout from '../MainLayout/MainLayout';
+import { AnimatedSwitch } from 'react-router-transition';
 
 const App = () => (
-  <MainLayout>
-    <BrowserRouter>
-      <Switch>
+  <BrowserRouter>
+    <MainLayout>
+      <AnimatedSwitch
+        atEnter={{ opacity: 0 }}
+        atLeave={{ opacity: 0 }}
+        atActive={{ opacity: 1 }}
+        className="switch-wrapper"
+      >
         <Route exact path="/" component={Home} />
         <Route exact path="/info" component={Info} />
-        <Route exact path="/faq" component={Faq} />
-      </Switch>
-    </BrowserRouter>
-  </MainLayout>
+        <Route exact patg="/info" component={Faq} />
+      </AnimatedSwitch>
+    </MainLayout>
+  </BrowserRouter>
 );
 
 export default App;
